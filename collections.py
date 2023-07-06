ed8c7619c50c8764fd743031ee38f444e60f687d
vverac <viverace@gmail.com>
2023-07-06 11:04:29 -0400

    archivos creados de aws cloud9

---
+++ collections.py
@@ -0,0 +1,38 @@
+# Definición de una lista
+myFruitList = ["apple", "banana", "cherry"]
+print(myFruitList)
+print(type(myFruitList))
+
+print(myFruitList[0])
+print(myFruitList[1])
+print(myFruitList[2])
+
+# Modificación de los valores de una lista
+myFruitList[2] = "orange"
+print(myFruitList)
+
+# Definición de una tupla
+myFinalAnswerTuple = ("apple", "banana", "pineapple")
+print(myFinalAnswerTuple)
+print(type(myFinalAnswerTuple))
+
+# Acceso a una tupla por posición
+print(myFinalAnswerTuple[0])
+print(myFinalAnswerTuple[1])
+print(myFinalAnswerTuple[2])
+
+# Presentar el tipo de dato de diccionario
+# Definición de un diccionario
+myFavoriteFruitDictionary = {
+  "Akua" : "apple",
+  "Saanvi" : "banana",
+  "Paulo" : "pineapple"
+}
+
+print(myFavoriteFruitDictionary)
+print(type(myFavoriteFruitDictionary))
+
+# Acceso al diccionario por nombre
+print(myFavoriteFruitDictionary["Akua"])
+print(myFavoriteFruitDictionary["Saanvi"])
+print(myFavoriteFruitDictionary["Paulo"])
---
+++ numeric-data.py
@@ -0,0 +1,26 @@
+print("Python has three numeric types: int, float, and complex")
+
+myValue=1
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue2=3.14
+print(myValue2)
+print(type(myValue2))
+print(str(myValue2) + " is of the data type " + str(type(myValue2)))
+
+myValue=5j
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=True
+print(myValue)
+print(type(myValue))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
+
+myValue=False
+print(myValue2)
+print(type(myValue2))
+print(str(myValue) + " is of the data type " + str(type(myValue)))
